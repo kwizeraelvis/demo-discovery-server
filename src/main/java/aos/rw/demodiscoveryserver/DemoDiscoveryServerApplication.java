f3591776f34916c7167cc9b25921ace80d8e9176
Kwizera Aime Elvis <elvis.kwizera@aos.rw>
2021-12-17 12:17:47 +0200

    Initalize Discovery Server with Native Image Support

---
+++ src/main/java/aos/rw/demodiscoveryserver/DemoDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package aos.rw.demodiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDiscoveryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aos/rw/demodiscoveryserver/DemoDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package aos.rw.demodiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
